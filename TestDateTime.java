fa7dbacfcc13e65a5b56691da0ba7bfef3c6a99f
AimanZaharin <92364588+AimanZaharin@users.noreply.github.com>
2022-04-19 09:12:31 +0800

    Add files via upload

---
+++ TestDateTime.java
@@ -0,0 +1,11 @@
+
+public class TestDateTime {
+
+	public static void main(String[] args) {
+
+		// this is how to print out real date and time
+		java.util.Date masa = new java.util.Date(); 
+		System.out.println(masa.toString());
+	}
+
+}
